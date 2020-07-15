69cc53ee5cde807c87c8e3fdbbb8ec281b8e3883
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-07-15 18:03:50 +0500

    3 files modified

--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -10,6 +10,48 @@ export class GalleryService {
   constructor(private http: HttpClient) { }
 
   public async fetchGallery(userId: string): Promise<object> {
-    return null;
+      const url = 'https://localhost:44310/api/image/imagelist';
+      const response = [];
+      const payload = {
+          UserGuid: userId,
+      };
+
+      await this.http.post(url, payload)
+      .toPromise()
+      .then(data => {
+        if (Array.isArray(data)) {
+          data.forEach((record)=> {
+            response.push({
+              imageName: record['imageName'],
+              imageDate: record['imageDate']
+            })
+          });
+        }
+      })
+      .catch(error => {})
+
+    // return response;
+    return this.loadFakeData();
+  }
+
+  private loadFakeData() {
+    return [
+      {
+        imageName: "https://picsum.photos/id/237/200/300",
+        imageDate: "2020-07-14T00:00:00"
+      },
+      {
+        imageName: "https://picsum.photos/seed/picsum/200/300",
+        imageDate: "2020-07-14T16:13:58.426896"
+      },
+      {
+        imageName: "https://picsum.photos/200/300.jpg",
+        imageDate: "2020-07-14T17:14:11.966182"
+      },
+      {
+        imageName: "https://picsum.photos/200/300/?blur",
+        imageDate: "2020-07-14T17:30:50.416952"
+      },
+    ];
   }
 }
