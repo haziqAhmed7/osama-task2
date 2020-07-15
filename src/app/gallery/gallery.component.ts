0b2112a8510c63d95162a5425f65f32a3704ab02
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-07-15 18:09:43 +0500

    1 file modified

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,13 +1,37 @@
 import {Component, OnInit, OnDestroy, }  from '@angular/core';
 import {UserService} from '../services/user.service';
+import {GalleryService} from '../services/gallery.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent{
+export class GalleryComponent implements OnInit{
+  
+  showGalleryLoading = true;
+  imagesGallery = [];
+
   constructor(
     private userService: UserService,
+    private gallery: GalleryService,
   ) {}
+
+  ngOnInit() {
+
+  }
+
+  private toggleShowGalleryLoading() {
+    this.showGalleryLoading = ! this.showGalleryLoading;
+  }
+
+  private async fetchGallery(): Promise<object>{
+    this.imagesGallery = [];
+    const response = await this.gallery.fetchGallery(
+      this.userService.getUser().userId
+      );
+     this.imagesGallery = (Array.isArray(response)) ? response : []; 
+    this.toggleShowGalleryLoading();
+    return null;
+  }
 }
