2ab34b093acb32e15dc93e747f478f6cc6009148
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-07-16 01:59:49 +0500

    2 files modified

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,29 +1,30 @@
-import {Component, OnInit, OnDestroy, }  from '@angular/core';
-import {UserService} from '../services/user.service';
-import {GalleryService} from '../services/gallery.service';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { UserService } from "../services/user.service";
+import { GalleryService } from "../services/gallery.service";
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  selector: "app-gallery",
+  templateUrl: "./gallery.component.html",
+  styleUrls: ["./gallery.component.css"]
 })
-export class GalleryComponent implements OnInit{
-  
+export class GalleryComponent implements OnInit {
   showGalleryLoading = true;
   showUploadForm = false;
+  isUploading = false;
   imagesGallery = [];
+  imagesUpload = [];
   userName: string;
 
   constructor(
     private userService: UserService,
-    private gallery: GalleryService,
+    private gallery: GalleryService
   ) {}
 
   ngOnInit() {
     this.userName = [
-      this.userService.getUser().firstName, 
+      this.userService.getUser().firstName,
       this.userService.getUser().lastName
-    ].join(' ');
+    ].join(" ");
     this.fetchGallery();
   }
 
@@ -32,26 +33,42 @@ export class GalleryComponent implements OnInit{
     this.toggleUploadForm();
   }
 
-  onSelectFile(event) {}
-  
+  onSelectFile(event) {
+    this.imagesUpload = [];
+     if (event.target.files && event.target.files[0]) {
+       const filesAmount = event.target.files.length;
+       for (let i=0; i< filesAmount; i++) {
+         this.imagesUpload.push(event.target.files[i]);
+       }
+     }
+  }
+
+  startUpload(){
+
+  }
+
   // private methods
-  private async fetchGallery(): Promise<object>{
+  private async fetchGallery(): Promise<object> {
     this.imagesGallery = [];
     const response = await this.gallery.fetchGallery(
       this.userService.getUser().userId
-      );
-     console.log(response); 
-     this.imagesGallery = (Array.isArray(response)) ? response : []; 
-     console.log(this.imagesGallery); 
-     this.toggleShowGalleryLoading();
-     return null;
+    );
+    console.log(response);
+    this.imagesGallery = Array.isArray(response) ? response : [];
+    console.log(this.imagesGallery);
+    this.toggleShowGalleryLoading();
+    return null;
   }
 
-   private toggleShowGalleryLoading() {
-    this.showGalleryLoading = ! this.showGalleryLoading;
+  private toggleShowGalleryLoading() {
+    this.showGalleryLoading = !this.showGalleryLoading;
   }
 
   private toggleUploadForm() {
-    this.showUploadForm = ! this.showUploadForm;
+    this.showUploadForm = !this.showUploadForm;
+  }
+
+  private toggleIsUploading() {
+    this.isUploading = ! this.isUploading;
   }
 }
